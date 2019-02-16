e302f19fbbd1a1ce5a2643b7e056df23c5246cf1
Gustavo <gortega@ualberta.ca>
2019-02-16 05:38:32 +0000

    Revert "Changed build files"
    This reverts commit 942b3976966d3fcfa226d721312c4c7cb3038eb2.

---
+++ precache-manifest.2fd3e5f9f226e94144de5c8d57b5112e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae35775934ebdc352cf9",
+    "url": "/static/js/main.ae357759.chunk.js"
+  },
+  {
+    "revision": "947d507a2eb31f2f56b4",
+    "url": "/static/js/1.947d507a.chunk.js"
+  },
+  {
+    "revision": "ae35775934ebdc352cf9",
+    "url": "/static/css/main.a22e3006.chunk.css"
+  },
+  {
+    "revision": "20932992d6d19810f83ecb6e2980b64f",
+    "url": "/index.html"
+  }
+];
